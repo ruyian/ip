ec42e0c3e7f58660fa414b28e52ed28d8e16fdb5
an ruyi <anruyi@u.nus.edu>
2021-08-25 18:46:34 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,40 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.printf("____________________________________________________________\n" +
-                "Hello! I'm Duke\n" +
-                "What can I do for you?\n" +
-                "____________________________________________________________\n"+
-                "Bye. Hope to see you again soon!\n"+
-                "____________________________________________________________");
+        try (Scanner sc = new Scanner(System.in)) {
+            String sentence;
+            Chat.greet();
+            while (true) {
+                sentence = sc.nextLine();
+                if (sentence.equals("bye")) {
+                    Chat.bye();
+                    break;
+                }
+                Chat.parrot(sentence);
+            }
+        }
+    }
+}
+
+class Chat {
+    static void greet() {
+        System.out.printf("____________________________________________________________%n" +
+                "Hello! I'm Duke, the parrot.%n" +
+                "What can I do for you?%n" +
+                "____________________________________________________________%n");
+    }
+
+    static void parrot(String sentence) {
+        System.out.printf("____________________________________________________________%n" +
+                sentence +
+                "%n____________________________________________________________%n");
+
+    }
+
+    static void bye() {
+        System.out.printf("____________________________________________________________%n" +
+                "Bye. Hope to see you again soon!%n" +
+                "____________________________________________________________%n");
     }
 }
