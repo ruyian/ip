ad3d1925e3f6e6898303bdade829dbde1a0f032f
an ruyi <anruyi@u.nus.edu>
2021-08-25 18:53:56 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,14 +10,20 @@ public class Duke {
                 if (sentence.equals("bye")) {
                     Chat.bye();
                     break;
+                } else if (sentence.equals("list")) {
+                    Chat.list();
+                } else {
+                    Chat.parrot(sentence);
                 }
-                Chat.parrot(sentence);
             }
         }
     }
 }
 
 class Chat {
+    static String[] texts = new String[100];
+    static int textIndex = 0;
+
     static void greet() {
         System.out.printf("____________________________________________________________%n" +
                 "Hello! I'm Duke, the parrot.%n" +
@@ -26,9 +32,10 @@ class Chat {
     }
 
     static void parrot(String sentence) {
-        System.out.printf("____________________________________________________________%n" +
+        System.out.printf("____________________________________________________________%nadded: " +
                 sentence +
                 "%n____________________________________________________________%n");
+        texts[textIndex++] = sentence;
 
     }
 
@@ -36,5 +43,14 @@ class Chat {
         System.out.printf("____________________________________________________________%n" +
                 "Bye. Hope to see you again soon!%n" +
                 "____________________________________________________________%n");
+    }
+
+    static void list() {
+        int i;
+        System.out.printf("____________________________________________________________%n");
+        for (i = 0; i < textIndex; i++) {
+            System.out.println((i + 1) + ". " + texts[i]);
+        }
+        System.out.printf("____________________________________________________________%n");
     }
 }
