33cd32d4647dd684b68c93f918c6332085a7c1b1
an ruyi <anruyi@u.nus.edu>
2021-09-28 20:38:18 +0800

    Add java docs

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -4,6 +4,12 @@ import duke.exception.IrregularInputException;
 import duke.task.Task;
 
 public class Parser {
+    /** Parses user input to corresponding command enum based on user input.
+     *
+     * @param sentence Raw String taken from user input
+     * @return an action to be taken by Command
+     * @throws DukeException including illegal inputs and empty inputs
+     */
     public static Action parseCommand(String sentence) throws DukeException {
         if (sentence.isEmpty()) {
             throw new EmptyInputException("Empty input! Try again (o|o)\n");
@@ -30,6 +36,12 @@ public class Parser {
         }
     }
 
+    /** Parse the index of the tasks the user wants to done / delete
+     *
+     * @param sentence the raw String from user input of "done" and "delete"
+     * @return targetIndex,
+     * @throws IrregularInputException if an int cannot be parsed
+     */
     public static int parseIndex(String sentence) throws IrregularInputException {
         String[] words = sentence.split(" ");
         try {
@@ -40,6 +52,12 @@ public class Parser {
         }
     }
 
+    /** Parse the keyword of the looks for
+     *
+     * @param sentence, raw user input
+     * @return the keyword that the user looks for
+     * @throws IrregularInputException if bad keyword or no keyword is keyyed in
+     */
     public static String parseKeyWord(String sentence) throws IrregularInputException {
         try {
             int spaceIndex = sentence.indexOf(' ');
@@ -55,6 +73,11 @@ public class Parser {
         }
     }
 
+    /** Parse the description of task from user input
+     *
+     * @param sentence raw user input
+     * @return the description of the task
+     */
     public static String parseDescription(String sentence) {
         int spaceIndex = sentence.indexOf(' ');
         String description = sentence.substring(spaceIndex + 1);
